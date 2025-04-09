a1fb7bcf4271ec0706a82e66e9a1f1cbd8e163e0
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-04-09 20:24:37 +0300

    render navlinks

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,23 +1,41 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+
+import logoImg from "../../public/logoNeo.png";
+
+const navLinks = [
+    { text: 'Home', path: '/' },
+    { text: 'Abouts Us', path: '/about-us' },
+    { text: 'Directions', path: '/directions' },
+    { text: 'Portfolio', path: '/portfolio' },
+    { text: 'Clients', path: '/clients' },
+    { text: 'Road Signs', path: '/road-signs' },
+    { text: 'Equipment', path: '/equipment' },
+    { text: 'Contacts', path: '/contacts' },
+];
 
 export default function MainNavigation() {
 
-    return <header className="w-full mx-auto py-8 px-20 flex justify-betweens bg-stone-950">
-        <nav className="flex flex-row gap-20">
+    return <header className="w-full h-24 px-20 flex flex-row justify-betweens bg-stone-900">
+        <div className="h-22 w-auto mt-1">
+            <img src={logoImg} alt="logo" className="h-full w-auto object-contain" />
+        </div>
+        <nav className="flex flex-row my-auto">
             <ul className="flex gap-4">
-                <li>
-                    <NavLink
-                        to="/"
-                        className={({ isActive }) =>
-                            isActive
-                                ? "text-[#BAE218] underline"
-                                : "text-[#dec245] hover:text-[#d9b15a] hover:underline"
-                        }
-                        end
-                    >
-                        <span className="text-xl font-semibold">Home</span>
-                    </NavLink>
-                </li>
+                {navLinks.map((navlink) => (
+                    <li key={navlink.text}>
+                        <NavLink
+                            to={navlink.path}
+                            className={({ isActive }) =>
+                                isActive
+                                    ? "text-[#BAE218] underline"
+                                    : "text-[#dec245] hover:text-[#d9b15a] hover:underline"
+                            }
+                            end
+                        >
+                            <span className="text-xl font-semibold">{navlink.text}</span>
+                        </NavLink>
+                    </li>
+                ))}
             </ul>
         </nav>
     </header>
